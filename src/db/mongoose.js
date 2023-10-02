6eb850150d463b9e509053b6a5d884f6577853d1
Alex Viola <alrv0157@gmail.com>
2023-10-02 19:33:18 -0400

    second commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-console.log(process.end)
-// mongoose.connect(process.env.MONGODB_URL, {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true
-// }).then(() => {
-//     console.log('Connected to mongoose!');
-// }).catch(err => {
-//     console.log(err);
-// })
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(() => {
+    console.log('Connected to mongoose!');
+}).catch(err => {
+    console.log(err);
+})
