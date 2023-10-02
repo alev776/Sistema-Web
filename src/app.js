e82d292d445c0d71fd05ae9af4454e894f655416
Alex Viola <alrv0157@gmail.com>
2023-10-02 15:29:27 -0400

    Fixes

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const publicDirectory = path.join(__dirname, '../public');
 
 app.use(express.json());
 //app.use(cors({origin: 'http://localhost:8080'}));
-app.use(cors({origin: 'https://sales-app-rd-b9050328b101.herokuapp.com/'}));
+app.use(cors({origin: 'https://sistema-web-rd-fbf35f5aec8a.herokuapp.com/'}));
 // app.use(cors({origin: 'https://web-ventas-app.herokuapp.com/'}));
 app.use(express.static(publicDirectory));
 app.use(login);
